3960beb6ef6c7c6287573d7042659b4cd9165fa9
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2023-08-06 20:09:51 +0300

    for checkin

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,29 @@
+import axios from "axios";
+
+// Встановлюємо заголовок з ключем доступу для всіх HTTP-запитів
+axios.defaults.headers.common["x-api-key"] = "live_DFj4gSN6S75s14sRPAM8T9OXF7l8YTyiNSxLqafGruuTWhPOIaRYo9UuF3VL0QAF";
 
 
-export function fetchBreeds() {
-    return axios
-      .get("https://api.thecatapi.com/v1/breeds")
-      .then((response) => response.data)
-      .catch((error) => {
-        throw new Error("Failed to fetch breeds.");
-      });
+// Функція для здійснення HTTP-запиту за списком порід котів
+export async function fetchBreeds() {
+  try {
+    const response = await axios.get("https://api.thecatapi.com/v1/breeds");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching cat breeds:", error);
+    throw error;
   }
-  
-  export function fetchCatByBreed(breedId) {
-    return axios
-      .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-      .then((response) => response.data[0])
-      .catch((error) => {
-        throw new Error("Failed to fetch cat by breed.");
-      });
+}
+console.log(fetchBreeds());
+
+// Функція для здійснення HTTP-запиту за інформацією про кота по ідентифікатору породи
+export async function fetchCatByBreed(breedId) {
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  try {
+    const response = await axios.get(url);
+    return response.data[0];
+  } catch (error) {
+    console.error("Error fetching cat info:", error);
+    throw error;
   }
+}
--- src/index.js
+++ src/index.js
@@ -1,79 +1,55 @@
-import axios from "axios";
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 
-const API_KEY = "тlive_DFj4gSN6S75s14sRPAM8T9OXF7l8YTyiNSxLqafGruuTWhPOIaRYo9UuF3VL0QAF";
-axios.defaults.headers.common["x-api-key"] = API_KEY;
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
 
-
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-
-const breedSelect = document.querySelector(".breed-select");
-const loader = document.querySelector(".loader");
-const error = document.querySelector(".error");
-const catInfo = document.querySelector(".cat-info");
-
-function showLoader() {
-  loader.style.display = "block";
-  breedSelect.style.display = "none";
-  error.style.display = "none";
-  catInfo.style.display = "none";
+// Функція для показу/приховування елементів на сторінці
+function toggleElements(isLoading) {
+  breedSelect.style.display = isLoading ? 'none' : 'block';
+  loader.style.display = isLoading ? 'block' : 'none';
+  error.style.display = 'none';
+  catInfo.style.display = 'none';
 }
+// console.log(toggleElements(isLoading));
 
-function hideLoader() {
-  loader.style.display = "none";
-}
+// Завантаження списку порід котів при завантаженні сторінки
+document.addEventListener('DOMContentLoaded', async () => {
+  toggleElements(true);
+  try {
+    const breeds = await fetchBreeds();
+    breedSelect.innerHTML = breeds.map(breed => `<option value="${breed.id}">${breed.name}</option>`).join('');
+    toggleElements(false);
+  } catch (error) {
+    console.error('Error fetching cat breeds:', error);
+    toggleElements(false);
+    Notiflix.Notify.failure('❌ Error fetching cat breeds');
+  }
+});
 
-function showError() {
-  error.style.display = "block";
-}
+// Запит за інформацією про кота при виборі породи у селекті
+breedSelect.addEventListener('change', async () => {
+  toggleElements(true);
+  const breedId = breedSelect.value;
+  try {
+    const catData = await fetchCatByBreed(breedId);
+    const { name, description, temperament } = catData.breeds[0];
+    const imageSrc = catData.url;
 
-function hideError() {
-  error.style.display = "none";
-}
+    catInfo.innerHTML = `
+      <h2>${name}</h2>
+      <p><strong>Description:</strong> ${description}</p>
+      <p><strong>Temperament:</strong> ${temperament}</p>
+      <img src="${imageSrc}" alt="${name}" />
+    `;
 
-function updateCatInfo(catData) {
-  catInfo.innerHTML = `
-    <img src="${catData.url}" alt="${catData.breeds[0].name}" />
-    <h2>${catData.breeds[0].name}</h2>
-    <p><strong>Description:</strong> ${catData.breeds[0].description}</p>
-    <p><strong>Temperament:</strong> ${catData.breeds[0].temperament}</p>
-  `;
-  catInfo.style.display = "block";
-}
-
-function populateBreedSelect(breeds) {
-  breeds.forEach((breed) => {
-    const option = document.createElement("option");
-    option.value = breed.id;
-    option.textContent = breed.name;
-    breedSelect.appendChild(option);
-  });
-  breedSelect.style.display = "block";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  showLoader();
-  fetchBreeds()
-    .then((breeds) => {
-      populateBreedSelect(breeds);
-      hideLoader();
-    })
-    .catch((error) => {
-      hideLoader();
-      showError();
-    });
-
-  breedSelect.addEventListener("change", () => {
-    const selectedBreedId = breedSelect.value;
-    showLoader();
-    hideError();
-    fetchCatByBreed(selectedBreedId)
-      .then((catData) => {
-        updateCatInfo(catData);
-        hideLoader();
-      })
-      .catch((error) => {
-        hideLoader();
-        showError();
-      });
-  });
+    catInfo.style.display = 'block';
+    toggleElements(false);
+  } catch (error) {
+    console.error('Error fetching cat info:', error);
+    toggleElements(false);
+    Notiflix.Notify.failure('❌ Error fetching cat info');
+  }
 });
